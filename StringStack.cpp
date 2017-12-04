48304a2f40e686e39c72fa84792ed10fb4599429
NickElliot <oavatosdk@gmail.com>
2017-12-04 11:50:02 -0800

    explanatory comment added to StringStack.cpp

--- StringStack.cpp
+++ StringStack.cpp
@@ -7,5 +7,12 @@ void push(std::string) {
 
 }
 bool isEmpty() {
-    
+
 }
+
+
+/*Unfortunately I do not know how to implement this data structure in C++. 
+  At my old school we worked in Java up through data structures and I have never worked in C++ in a meaningful capacity before this class.
+  I could probably figure out how to adapt my knowledge given more time but due to the nature of this quiz that time doesn't exactly exist.
+  The conceptual idea would be creating the stack via an object with nodes, variables, and iterators but anything I could quickly churn out
+  I'm certain would not be functionable right now.*/
